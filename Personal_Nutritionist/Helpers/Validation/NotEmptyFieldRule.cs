e997c59754caf4f550a0a901d3f6f537438dec08
AntonBorisovArcuman <anton.borisov@dws.la>
2022-05-13 09:34:13 +0300

    add helpers

---
+++ Personal_Nutritionist/Helpers/Validation/NotEmptyFieldRule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Personal_Nutritionist.Helpers.Validation
+{
+    public class NotEmptyFieldRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                return string.IsNullOrWhiteSpace((value ?? "").ToString()) ? new ValidationResult(false, "Field is required and must be not empty")
+                : ValidationResult.ValidResult;
+            }
+            catch
+            {
+                MessageBox.Show("Can't check entered value");
+                return null;
+            }
+        }
+    }
+}
